379ac11719a9fb28e4ffc1fdded3dc35828330ef
Diego <diegoguatedb2002@gmail.com>
2021-07-21 11:34:55 -0600

    Se agregó al segundo jugador

---
+++ BlueSpaceship.java
@@ -0,0 +1,17 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class BlueSpaceship here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class BlueSpaceship extends Spaceship
+{
+    
+    public BlueSpaceship(){
+        super("w","a","d", "spaceShip2.png", "astronaut2.png");
+    }
+    
+    
+}
